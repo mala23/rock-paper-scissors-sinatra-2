1f50b94979c1fa8ac958d64f0c359d4491274e25
mala23 <mail@mala23.me>
2014-10-21 08:05:59 +0100

    create sinatra app

---
+++ app/server.rb
@@ -0,0 +1,16 @@
+require 'sinatra/base'
+require 'haml'
+require 'bundler/setup'
+
+class RpsSinatra2 < Sinatra::Base
+  
+  enable 'sessions'
+  set :session_secret, 's3cr3t'
+  
+  get '/' do
+    'Hello RpsSinatra2!'
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
